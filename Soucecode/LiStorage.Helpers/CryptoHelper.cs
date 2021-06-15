bb1f4dea72c81c422ed9ba85dbb6cbb8d088f30f
Lennie Wennerlund <lennie@lempa.se>
2021-06-15 15:50:57 +0200

    wip

---
+++ Soucecode/LiStorage.Helpers/CryptoHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.IO;
+using System.Text;
+
+namespace LiStorage.Helpers
+{
+    public static class CryptoHelper
+    {
+        public static string Md5(byte[] data)
+        {
+            if (data == null) return null;
+
+            MD5 md5 = MD5.Create();
+            byte[] hash = md5.ComputeHash(data);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++) sb.Append(hash[i].ToString("X2"));
+            string ret = sb.ToString();
+            return ret;
+        }
+
+        public static string Md5(string data)
+        {
+            if (String.IsNullOrEmpty(data)) return null;
+
+            MD5 md5 = MD5.Create();
+            byte[] dataBytes = System.Text.Encoding.ASCII.GetBytes(data);
+            byte[] hash = md5.ComputeHash(dataBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++) sb.Append(hash[i].ToString("X2"));
+            string ret = sb.ToString();
+            return ret;
+        }
+
+        public static string Md5(Stream stream)
+        {
+            if (stream == null || !stream.CanRead) return null;
+
+            MD5 md5 = MD5.Create();
+            byte[] hash = md5.ComputeHash(stream);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++) sb.Append(hash[i].ToString("X2"));
+            string ret = sb.ToString();
+            return ret;
+        }
+    }
+}
---
+++ Soucecode/LiStorage.Models/Rundata/NodeStartUpStatusModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiStorage.Models.Rundata
+{
+    public class NodeStartUpStatusModel
+    {
+        public NodeStartUpStatusEnum ConfigFileExist { get; set; }
+        public NodeStartUpStatusEnum ConfigFileRead { get; set; }
+        public NodeStartUpStatusEnum ConfigFileWorking { get; set; }
+
+        public NodeStartUpStatusModel()
+        {
+            this.ConfigFileExist = NodeStartUpStatusEnum.Notstarted;
+            this.ConfigFileRead = NodeStartUpStatusEnum.Notstarted;
+            this.ConfigFileWorking = NodeStartUpStatusEnum.Notstarted;
+        }
+    }
+
+    public enum NodeStartUpStatusEnum { Notstarted = 0, Running = 1, Error = 2, Done = 3 }
+}
