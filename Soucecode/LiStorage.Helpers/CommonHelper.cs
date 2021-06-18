23298404247a8253509d543e8a8b327072391580
Lennie Wennerlund <lennie@lempa.se>
2021-06-18 19:48:39 +0200

    ContainsUnsafeCharacters, ContainsUnsafeCharacters, RandomString added

--- Soucecode/LiStorage.Helpers/CommonHelper.cs
+++ Soucecode/LiStorage.Helpers/CommonHelper.cs
@@ -80,7 +80,120 @@ namespace LiStorage.Helpers
 
         #endregion
 
+        #region String safe to use
 
+        public static bool ContainsUnsafeCharacters(string data)
+        {
+            /*
+             * 
+             * Returns true if unsafe characters exist
+             * 
+             * 
+             */
+
+            // see https://kb.acronis.com/content/39790
+
+            if (String.IsNullOrEmpty(data)) return false;
+            if (data.Equals(".")) return true;
+            if (data.Equals("..")) return true;
+
+            if (
+                (String.Compare(data.ToLower(), "com1") == 0) ||
+                (String.Compare(data.ToLower(), "com2") == 0) ||
+                (String.Compare(data.ToLower(), "com3") == 0) ||
+                (String.Compare(data.ToLower(), "com4") == 0) ||
+                (String.Compare(data.ToLower(), "com5") == 0) ||
+                (String.Compare(data.ToLower(), "com6") == 0) ||
+                (String.Compare(data.ToLower(), "com7") == 0) ||
+                (String.Compare(data.ToLower(), "com8") == 0) ||
+                (String.Compare(data.ToLower(), "com9") == 0) ||
+                (String.Compare(data.ToLower(), "lpt1") == 0) ||
+                (String.Compare(data.ToLower(), "lpt2") == 0) ||
+                (String.Compare(data.ToLower(), "lpt3") == 0) ||
+                (String.Compare(data.ToLower(), "lpt4") == 0) ||
+                (String.Compare(data.ToLower(), "lpt5") == 0) ||
+                (String.Compare(data.ToLower(), "lpt6") == 0) ||
+                (String.Compare(data.ToLower(), "lpt7") == 0) ||
+                (String.Compare(data.ToLower(), "lpt8") == 0) ||
+                (String.Compare(data.ToLower(), "lpt9") == 0) ||
+                (String.Compare(data.ToLower(), "con") == 0) ||
+                (String.Compare(data.ToLower(), "nul") == 0) ||
+                (String.Compare(data.ToLower(), "prn") == 0) ||
+                (String.Compare(data.ToLower(), "con") == 0)
+                )
+            {
+                return true;
+            }
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (
+                    ((int)(data[i]) < 32) ||    // below range
+                    ((int)(data[i]) > 126) ||   // above range
+                    ((int)(data[i]) == 47) ||   // slash /
+                    ((int)(data[i]) == 92) ||   // backslash \
+                    ((int)(data[i]) == 63) ||   // question mark ?
+                    ((int)(data[i]) == 60) ||   // less than < 
+                    ((int)(data[i]) == 62) ||   // greater than >
+                    ((int)(data[i]) == 58) ||   // colon :
+                    ((int)(data[i]) == 42) ||   // asterisk *
+                    ((int)(data[i]) == 124) ||  // pipe |
+                    ((int)(data[i]) == 34) ||   // double quote "
+                    ((int)(data[i]) == 39) ||   // single quote '
+                    ((int)(data[i]) == 94)      // caret ^
+                    )
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool ContainsUnsafeCharacters(List<string> data)
+        {
+            if (data == null || data.Count < 1) return true;
+            foreach (string curr in data)
+            {
+                if (ContainsUnsafeCharacters(curr)) return true;
+            }
+            return false;
+        }
+
+        #endregion
+
+        #region Random
+
+        public static string RandomString(int numChar)
+        {
+            string ret = "";
+            if (numChar < 1) return "";
+            int valid = 0;
+            Random random = new Random((int)DateTime.Now.Ticks);
+            int num = 0;
+
+            for (int i = 0; i < numChar; i++)
+            {
+                num = 0;
+                valid = 0;
+                while (valid == 0)
+                {
+                    num = random.Next(126);
+                    if (((num > 47) && (num < 58)) ||
+                        ((num > 64) && (num < 91)) ||
+                        ((num > 96) && (num < 123)))
+                    {
+                        valid = 1;
+                    }
+                }
+                ret += (char)num;
+            }
+
+            return ret;
+        }
+
+
+        #endregion
 
     }
 }
