14ed764c5868a25738a6053201e1678726c3faa4
Lennie Wennerlund <lennie@lempa.se>
2021-06-18 21:00:51 +0200

    wip

---
+++ Soucecode/LiStorage.Models/StoragePool/StoragePoolEnums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiStorage.Models.StoragePool
+{
+
+    public enum StoragePoolStatusEnum { Nodata, Init, FileMissing, Error, Warning, Working }
+
+    public enum StoragePoolTypesEnum
+    {
+        None = 0,
+        Singel = 1,
+        Raid0 = 2,
+        Raid1 = 3,
+        Raid5 = 4,
+        Raid6 = 5,
+        Raid10 = 6
+    }
+
+}
