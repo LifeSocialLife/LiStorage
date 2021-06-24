27099ff3dfe211bbf9b6cf2c9ae9d2bcb89ad058
Lennie Wennerlund <lennie@lempa.se>
2021-06-24 10:08:31 +0200

    dokumentation added

--- Soucecode/LiStorage.Models/StoragePool/StoragePoolEnums.cs
+++ Soucecode/LiStorage.Models/StoragePool/StoragePoolEnums.cs
@@ -1,21 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// <summary>
+// Enums for Storage pools
+// </summary>
+// <copyright file="StoragePoolEnums.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+// <author>Lennie Wennerlund (lempa)</author>
 
 namespace LiStorage.Models.StoragePool
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
 
-    public enum StoragePoolStatusEnum { Nodata, Init, FileMissing, Error, Warning, Working }
-
-    public enum StoragePoolTypesEnum
+    /// <summary>
+    /// Status of the storage pools.
+    /// </summary>
+    public enum StoragePoolStatusEnum
     {
-        None = 0,
-        Singel = 1,
-        Raid0 = 2,
-        Raid1 = 3,
-        Raid5 = 4,
-        Raid6 = 5,
-        Raid10 = 6
+        /// <summary>Storage pool has not status.</summary>
+        Nodata = 0,
+
+        /// <summary>Storage pool is in init stage.</summary>
+        Init = 1,
+
+        /// <summary>Storage pool has filemissing status.</summary>
+        FileMissing = 2,
+
+        /// <summary>Storage pool is in error mode.</summary>
+        Error = 3,
+
+        /// <summary>Storage pool is in warning mode.</summary>
+        Warning = 4,
+
+        /// <summary>Storage pool is Working.</summary>
+        Working = 5,
     }
 
+    /// <summary>
+    /// Types of storage pools that exist in LiStorage software.
+    /// </summary>
+    public enum StoragePoolTypesEnum
+    {
+        /// <summary>Storage pool type not set.</summary>
+        None = 0,
+
+        /// <summary>Singel disk storage pool.</summary>
+        Singel = 1,
+
+        /// <summary>Raid 0 Disk storage (Stripe set).</summary>
+        Raid0 = 2,
+
+        /// <summary>Raid 1 Disk storage (Mirror set).</summary>
+        Raid1 = 3,
+
+        /// <summary>Raid 5 Disk storage (block-level striping with distributed parity - 1 parity disks).</summary>
+        Raid5 = 4,
+
+        /// <summary>Raid 6 Disk storage (block-level striping with distributed parity - 2 parity disks).</summary>
+        Raid6 = 5,
+
+        /// <summary>Raid 10 Disk storage (Disk mirror whit disk Stripe set).</summary
+        Raid10 = 6,
+    }
 }
