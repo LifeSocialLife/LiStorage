e5390e3acddb231fc1483dcfb5e57a090fed81cd
Lennie Wennerlund <lennie@lempa.se>
2021-07-03 10:48:05 +0200

    wip

--- Soucecode/LiStorage.Gui/Startup.cs
+++ Soucecode/LiStorage.Gui/Startup.cs
@@ -1,37 +1,72 @@
-using LiStorage.Gui.Data;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+// <copyright file="Startup.cs" company="LiSoLi">
+// Copyright (c) LiSoLi. All rights reserved.
+// </copyright>
 
 namespace LiStorage.Gui
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using LiStorage.Gui.Data;
+    using LiStorage.Services;
+    using LiStorage.Services.Node;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Components;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.HttpsPolicy;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    /// <summary>
+    /// Gui project startup class.
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">IConfiguration.</param>
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.Configuration = configuration;
         }
 
+        /// <summary>
+        /// Gets iConfiguration part.
+        /// </summary>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /// <summary>
+        /// ConfigureServices.
+        /// </summary>
+        /// <param name="services">IServiceCollection.</param>
         public void ConfigureServices(IServiceCollection services)
         {
+            // LiStorage backend services
+            services.AddSingleton<FileOperationService>();
+            services.AddSingleton<RundataService>();
+            services.AddSingleton<RundataNodeService>();
+            services.AddSingleton<CollectionService>();
+            services.AddSingleton<StoragePoolService>();
+            services.AddSingleton<BlockStorageService>();
+            services.AddSingleton<NodeHttpService>();
+
+            // Blazor default services
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            // Background services
+            services.AddHostedService<Worker>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// Configure part.
+        /// </summary>
+        /// <param name="app">IApplicationBuilder.</param>
+        /// <param name="env">IWebHostEnvironment.</param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -41,6 +76,7 @@ namespace LiStorage.Gui
             else
             {
                 app.UseExceptionHandler("/Error");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
--- Soucecode/LiStorage/Worker.cs
+++ Soucecode/LiStorage/Worker.cs
@@ -1,27 +1,68 @@
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
+// <summary>
+// Main handler for all background work, node and master.
+// </summary>
+// <copyright file="Worker.cs" company="LiSoLi">
+// Copyright (c) LiSoLi. All rights reserved.
+// </copyright>
 
 namespace LiStorage
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using LiStorage.Services;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// Listorage background worker.
+    /// </summary>
     public class Worker : BackgroundService
     {
+#pragma warning disable SA1309 // FieldNamesMustNotBeginWithUnderscore
         private readonly ILogger<Worker> _logger;
+        private readonly RundataService _rundata;
+        private readonly IConfiguration _configuration;
+#pragma warning restore SA1309 // FieldNamesMustNotBeginWithUnderscore
 
-        public Worker(ILogger<Worker> logger)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Worker"/> class.
+        /// </summary>
+        /// <param name="logger">ILogger.</param>
+        /// <param name="configuration">IConfiguration.</param>
+        /// <param name="rundataService">RundataService.</param>
+        public Worker(ILogger<Worker> logger, IConfiguration configuration, RundataService rundataService)
         {
-            _logger = logger;
+            this.zzDebug = "Worker";
+
+            this._logger = logger;
+            this._configuration = configuration;
+            this._rundata = rundataService;
         }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "Reviewed.")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Reviewed.")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Reviewed.")]
+        private string zzDebug { get; set; }
+
+        /// <inheritdoc/>
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            var dd = WebHostDefaults.ContentRootKey;
+            var d2 = this._configuration.GetValue<string>(WebHostDefaults.ContentRootKey);
+
+            this._rundata.Folders.PathRuntimes = this._configuration.GetValue<string>(WebHostDefaults.ContentRootKey);
+            this.zzDebug = "dfdsf";
+
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                this._logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 try
                 {
                     await Task.Delay(1000, stoppingToken);
