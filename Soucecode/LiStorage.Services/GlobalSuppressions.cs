028ee1636ac8e66a556187f2a42a379e93d35d64
Lennie Wennerlund <lennie@lempa.se>
2021-10-06 22:05:48 +0200

    code cleanup

--- Soucecode/LiStorage.Services/GlobalSuppressions.cs
+++ Soucecode/LiStorage.Services/GlobalSuppressions.cs
@@ -1,7 +1,9 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
+﻿// <summary>
+// CodeAnalysis suppressmessage file.
+// </summary>
+// <copyright file="GlobalSuppressions.cs" company="LiSoLi">
+// Copyright (c) LiSoLi. All rights reserved.
+// </copyright>
 
 using System.Diagnostics.CodeAnalysis;
 
